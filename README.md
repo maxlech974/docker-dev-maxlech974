6f612e7c16f1a319f7b702b7104de96be9deda2d
maxlech974 <maxime.lechere@gmail.com>
2021-05-02 23:45:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Environnement de dev ready to use!
+
+Environnemnt PHP 7.4 / apache2 ready to use
---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+install_dir="$HOME/.docker-dev-maxlech974"
+
+rm -rf $install_dir || true
+
+which docker
+
+if [ $? > 0 ]
+then
+    sudo apt-get remove docker docker-engine docker.io containerd runc && \
+    sudo apt-get update && \
+    sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+fi
+
+sh_aliases="${install_dir}/sh_aliases"
+
+which zsh
+if [ $? == 0 ]
+then 
+    grep -q  'docker-dev' ~/.zshrc || ln -s $sh_aliases $HOME/.oh-my-zsh/custom/docker-dev-maxlechere974.zsh
+fi
+
+which bash
+if [ $? == 0 ]
+then 
+    grep -q  'docker-dev' ~/.bashrc || echo "source $sh_aliases" >> ~/.bashrc
+fi
+
+
+
+
+my_current_path=$PWD 
+cd $HOME
+
+cp -r $my_current_path $install_dir
+rm -rf $my_current_path
+
+rm $install_dir/install.sh
+
+
+
+
+
